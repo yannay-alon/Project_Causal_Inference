c39fef5a2d844f71d1b77eb85e90edd9b119fb61
yannay-alon <62138811+yannay-alon@users.noreply.github.com>
2022-07-23 17:46:22 +0300

    Adding Data (low dim)

--- Data.py
+++ Data.py
@@ -6,5 +6,6 @@ def read_data(folder_path: str, dataset_path: str, group: Tuple[int, ...]) -> Tu
     all_files = [rf"{folder_path}\{dataset_path}{num}.csv" for num in group]
     data = pd.concat((pd.read_csv(f) for f in all_files), ignore_index=True)
 
-    ate = pd.read_csv(rf"{folder_path}\{dataset_path}{group[0]}_cf.csv")["ATE"].mean()
+    ground_truth_suffix = "_cf"
+    ate = pd.read_csv(rf"{folder_path}\{dataset_path}{group[0]}{ground_truth_suffix}.csv")["ATE"].mean()
     return data, ate
